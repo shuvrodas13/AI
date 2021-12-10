a82ddf2faa7206efd7472e3a78ecc60eebed0db8
Shuvro Das <shuvro6589@gmail.com>
2021-12-10 22:12:39 +0600

    wishme function check

--- ruby.py
+++ ruby.py
@@ -1,13 +1,29 @@
 import pyttsx3
+import datetime
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-print(voices[1].id)
 engine.setProperty('voice', voices[1].id)
 
 
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
+    
+    
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon")
+    else:
+        speak("Good Evening!") 
+        
+    speak(" I am Ruby sir. Please tell me how may I help you")       
+        
 if __name__ == "__main__":
-    speak("Diponkor is a good boy")
+    speak("")
+    if __name__ == "__main__":
+        wishMe()
+    
