615036cb2a92f1db9eab27cb7fa7e2e07eb16c8b
Shuvro Das <shuvro6589@gmail.com>
2021-12-11 23:47:57 +0600

    defining takeCommand

--- ruby.py
+++ ruby.py
@@ -1,4 +1,5 @@
 import pyttsx3
+import speech_recognition as sr
 import datetime
 
 engine = pyttsx3.init('sapi5')
@@ -21,9 +22,27 @@ def wishMe():
         speak("Good Evening!") 
         
     speak(" I am Ruby sir. Please tell me how may I help you")       
+
+def takeCommand():
+    r= sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold =1
+        audio = r.listen(source)
         
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query} \n)")
+    except Exception as e:
+        # print(e)
+        print("Say that again please...")
+        return "None"
+    return query
+
 if __name__ == "__main__":
     speak("")
     if __name__ == "__main__":
         wishMe()
+        takeCommand()
     
