f8a40be944a2b2f70b16231e160f3721ce26eed7
Shuvro Das <shuvro6589@gmail.com>
2021-12-12 17:59:13 +0600

    add wikipedia

--- ruby.py
+++ ruby.py
@@ -1,5 +1,7 @@
 import pyttsx3
+import speech_recognition as sr
 import datetime
+import wikipedia
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -21,9 +23,33 @@ def wishMe():
         speak("Good Evening!") 
         
     speak(" I am Ruby sir. Please tell me how may I help you")       
+
+def takeCommand():
+    r= sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold =1
+        audio = r.listen(source)
         
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query} \n)")
+    except Exception as e:
+        # print(e)
+        print("Say that again please...")
+        return "None"
+    return query
+
 if __name__ == "__main__":
-    speak("")
-    if __name__ == "__main__":
         wishMe()
-    
+        while True:
+            query = takeCommand().lower()
+            
+            if 'wikipedia' in query:
+                speak('Searching Wikipedia...')
+            query= query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=1)
+            speak("According to wikipedia...")
+            print(results)
+            speak(results)
